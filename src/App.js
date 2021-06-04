b8a7214737b35912db58a517e13b7111d3a06106
Yogeshwar Chaturvedi <kanhay8@gmail.com>
2021-06-04 13:39:50 +0530

    Added Crypto Prices

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+
 import './App.css';
 import Header from "./Header";
+import CryptoPrices from './CryptoPrices';
+
 
 function App() {
-
-  const [coins, setCoins] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get("https://api.coingecko.com/api/v3/coins/list")
-      .then(res => {
-        setCoins(res.data);
-        console.log(coins);
-      })
-      .catch(err => console.log("Error! Try again later."))
-  }, []);
 
   return (
     <div className="app">
       <Header></Header>
+      <CryptoPrices></CryptoPrices>
     </div>
   );
 }
--- src/Coin.js
+++ src/Coin.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
-const Coin = () => {
+const Coin = ({ name, image, symbol, price, volume, priceChange }) => {
     return (
-        <div className="header">
-
+        <div className="coin-container">
+            <div className="coin-row">
+                <div className="coin">
+                    <img src={image} alt="crypto" />
+                    <h1>{name}</h1>
+                    <p className="coin-symbol">{symbol}</p>
+                </div>
+                <div className="coin-data">
+                    <p className="coin-price">₹{price.toLocaleString()}</p>
+                    <div className="coin-volume">₹{volume.toLocaleString()}</div>
+                    {priceChange < 0 ? (
+                        <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
+                    ) : (<p className="coin-percent green">{priceChange.toFixed(2)}%</p>)
+                    }
+                </div>
+            </div>
         </div>
     )
 }
