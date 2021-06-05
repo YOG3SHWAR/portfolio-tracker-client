1831e43e7cb97621726783e097af9464eab69e0c
Yogeshwar Chaturvedi <kanhay8@gmail.com>
2021-06-05 20:06:32 +0530

    Adding Chars

--- src/components/CryptoPrices.js
+++ src/components/CryptoPrices.js
@@ -2,20 +2,25 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import '../css/CryptoPrices.css'
 import '../css/Coin.css'
+import '../css/Header.css'
 import Coin from './Coin'
+import { Link } from 'react-router-dom'
 
 const CryptoPrices = () => {
     const [coins, setCoins] = useState([]);
     const [search, setSearch] = useState('');
+    const [isLoading, setLoading] = useState(false);
 
     useEffect(() => {
+        setLoading(true)
         axios
             .get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false")
             .then(res => {
                 setCoins(res.data);
-                console.log(coins);
+
             })
-            .catch(err => console.log("Error! Try again later."))
+            .catch(err => console.log("Error! Try again later."));
+        setLoading(false);
     }, []);
 
     const handleChange = e => {
@@ -28,6 +33,11 @@ const CryptoPrices = () => {
         coin.symbol.toLowerCase().includes(search.toLowerCase())
     );
 
+    const setCoinId = (coinId) =>
+        localStorage.setItem("coinId", coinId);
+
+    if (isLoading) return (<div className="loading"></div>)
+
     return (
         <div className='crypto-prices'>
             <div className="coin-search">
@@ -39,17 +49,18 @@ const CryptoPrices = () => {
 
             {filteredCoins.map(coin => {
                 return (
-                    <Coin
-                        key={coin.id}
-                        name={coin.name}
-                        image={coin.image}
-                        symbol={coin.symbol}
-                        maketcap={coin.market_cap}
-                        price={coin.current_price}
-                        priceChange={coin.price_change_percentage_24h}
-                        volume={coin.total_volume}
-                    />
-
+                    <Link className="coin-link" to={`/coin-chart/${coin.id}`}>
+                        <Coin
+                            key={coin.id}
+                            name={coin.name}
+                            image={coin.image}
+                            symbol={coin.symbol}
+                            maketcap={coin.market_cap}
+                            price={coin.current_price}
+                            priceChange={coin.price_change_percentage_24h}
+                            volume={coin.total_volume}
+                        />
+                    </Link>
                 )
             })}
         </div>
