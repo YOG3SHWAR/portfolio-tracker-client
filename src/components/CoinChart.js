6c8ede21753a12239937cecc66ee04a7edec7894
Yogeshwar Chaturvedi <kanhay8@gmail.com>
2021-06-06 15:19:54 +0530

    Adding Charts

--- src/components/CoinChart.js
+++ src/components/CoinChart.js
@@ -2,23 +2,83 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
 import '../css/CoinChart.css'
 import axios from "axios";
+import Charts from './Charts';
 
 const CoinChart = () => {
     const id = useParams();
     const [coinData, setCoinData] = useState({});
 
+    const getDay = async () => {
+        try {
+            let res = await axios.get(`https://api.coingecko.com/api/v3/coins/${id.coinId}/market_chart?vs_currency=inr&days=1`)
+            setCoinData(prevState => ({
+                coinData: {
+                    ...prevState.coinData,
+                    day: res.data.prices
+                }
+            }))
+        }
+        catch (err) {
+            console.log(err);
+        }
+    };
+
+    const getWeek = async () => {
+        try {
+            let res = await axios.get(`https://api.coingecko.com/api/v3/coins/${id.coinId}/market_chart?vs_currency=inr&days=7`)
+            setCoinData(prevState => ({
+                coinData: {
+                    ...prevState.coinData,
+                    week: res.data.prices
+                }
+            }))
+        }
+        catch (err) {
+            console.log("Error! Try again later.");
+        }
+    };
+
+    const getMonth = async () => {
+        try {
+            let res = await axios.get(`https://api.coingecko.com/api/v3/coins/${id.coinId}/market_chart?vs_currency=inr&days=30`)
+            setCoinData(prevState => ({
+                coinData: {
+                    ...prevState.coinData,
+                    month: res.data.prices
+                }
+            }))
+        }
+        catch (err) {
+            console.log("Error! Try again later.");
+        }
+    };
+
+    const getYear = async () => {
+        try {
+            let res = await axios.get(`https://api.coingecko.com/api/v3/coins/${id.coinId}/market_chart?vs_currency=inr&days=365`)
+            setCoinData(prevState => ({
+                coinData: {
+                    ...prevState.coinData,
+                    year: res.data.prices
+                }
+            }))
+        }
+        catch (err) {
+            console.log("Error! Try again later.");
+        }
+    };
+
     useEffect(() => {
-        axios
-            .get(`https://api.coingecko.com/api/v3/coins/${id.coinId}/market_chart?vs_currency=inr&days=7`)
-            .then(res => {
-                setCoinData({ ...coinData, week: res.data.prices })
-            })
-            .catch(err => console.log("Error! Try again later."));
+        getDay();
+        getWeek();
+        getMonth();
+        getYear();
     }, [])
 
-
+    console.log("coinjdata", coinData);
     return (
         <div>
+            <Charts></Charts>
         </div>
     )
 }
--- src/components/CryptoPrices.js
+++ src/components/CryptoPrices.js
@@ -4,28 +4,25 @@ import '../css/CryptoPrices.css'
 import '../css/Coin.css'
 import '../css/Header.css'
 import Coin from './Coin'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const CryptoPrices = () => {
     const [coins, setCoins] = useState([]);
     const [search, setSearch] = useState('');
-    const getApi = async () =>
-      {
-        try
-          {
+    const getApi = async () => {
+        try {
             let res = await axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false")
             setCoins(res.data);
             console.log(coins);
-          }
-          catch(err)
-          {
+        }
+        catch (err) {
             console.log("Error! Try again later.");
-          }
-      };
-    const [isLoading, setLoading] = useState(false);
+        }
+    };
+    const [isLoading, setLoading] = useState(true);
 
     useEffect(() => {
-        setLoading(true)
+
         getApi();
         setLoading(false);
     }, []);
@@ -43,8 +40,34 @@ const CryptoPrices = () => {
     const setCoinId = (coinId) =>
         localStorage.setItem("coinId", coinId);
 
-    if (isLoading) return (<div className="loader"></div>)
 
+    const getLoad = () => {
+        if (isLoading) return (<div className="loader"></div>)
+        return (
+            <div>
+                {filteredCoins.map
+                    (
+                        coin => {
+                            return (
+                                <Link className="coin-link" to={`/coin-chart/${coin.id}`}>
+                                    <Coin
+                                        key={coin.id}
+                                        name={coin.name}
+                                        image={coin.image}
+                                        symbol={coin.symbol}
+                                        maketcap={coin.market_cap}
+                                        price={coin.current_price}
+                                        priceChange={coin.price_change_percentage_24h}
+                                        volume={coin.total_volume}
+                                    />
+                                </Link>
+                            )
+                        }
+                    )
+                }
+            </div>
+        )
+    }
     return (
         <div className='crypto-prices'>
             <div className="coin-search">
@@ -53,44 +76,8 @@ const CryptoPrices = () => {
                     <input type="text" placeholder="Search" className="coin-input" onChange={handleChange} />
                 </form>
             </div>
-
-            {filteredCoins.map
-              (
-                coin =>
-                {
-                  return (
-                      <Coin
-                          key={coin.id}
-                          name={coin.name}
-                          image={coin.image}
-                          symbol={coin.symbol}
-                          maketcap={coin.market_cap}
-                          price={coin.current_price}
-                          priceChange={coin.price_change_percentage_24h}
-                          volume={coin.total_volume}
-                      />
-
-                  )
-              }
-          )
-        }
-            {filteredCoins.map(coin => {
-                return (
-                    <Link className="coin-link" to={`/coin-chart/${coin.id}`}>
-                        <Coin
-                            key={coin.id}
-                            name={coin.name}
-                            image={coin.image}
-                            symbol={coin.symbol}
-                            maketcap={coin.market_cap}
-                            price={coin.current_price}
-                            priceChange={coin.price_change_percentage_24h}
-                            volume={coin.total_volume}
-                        />
-                    </Link>
-                )
-            })}
-        </div>
+            {getLoad()}
+        </div >
     )
 }
 
